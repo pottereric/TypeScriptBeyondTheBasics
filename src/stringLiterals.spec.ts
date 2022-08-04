c13bb194859a7b3ed512517c119f4e3980c6347a
Eric Potter <ft@mailinator.com>
2022-08-03 22:06:09 -0400

    Add the Utility Types

--- src/stringLiterals.spec.ts
+++ src/stringLiterals.spec.ts
@@ -4,12 +4,15 @@ test("Can assign grade", () => {
     EnterGrade("Eric Potter", "B");
 })
 
-
 test("Cannot assign invalid grade", () => {
-    //EnterGrade("Eric Potter", "E"); // won't compile
+    // EnterGrade("Eric Potter", "E"); // won't compile
 })
 
 
 test("Can assign expanded grade", () => {
-    EnterExpandedGrade("Eric Potter", "B+");
+    EnterExpandedGrade("Eric Potter", "A+");
+})
+
+test("F- is not a valid grade", () => {
+    EnterExpandedGrade("Eric Potter", "F-");
 })
--- src/stringLiterals.ts
+++ src/stringLiterals.ts
@@ -1,17 +1,23 @@
 export type Grade = "A" | "B" | "C" | "D" | "F"
-type diceRoll = 1 | 2 | 3 | 4 | 5 | 6;
 
 export function EnterGrade(studentName : string, grade : Grade){
     // TODO - store grade for student
 }
+
+type diceRoll = 1 | 2 | 3 | 4 | 5 | 6;
 
 enum myGrade {
     A, B, C, D
 }
 
 // Template Literal Types
-type GradeModifiers = "+" | "" | "-"
-type expandedGrades = `${Grade}${GradeModifiers}`
+
+
+
+type GradeModifiers = "+" | "" | "-" 
+export type expandedGrades = `${Grade}${GradeModifiers}`
+
+export type correctExpandedGrades = Exclude< `${Grade}${GradeModifiers}` , "F+" | "F-">
 
 export function EnterExpandedGrade(studentName : string, grade : expandedGrades)
 {
