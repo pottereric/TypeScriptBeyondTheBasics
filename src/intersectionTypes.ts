a94dc25d9f87f4ab23f418aa695c23702782e541
Eric Potter <eric.b.potter@gmail.com>
2022-10-19 09:38:00 -0400

    clean up

--- src/intersectionTypes.ts
+++ src/intersectionTypes.ts
@@ -1,19 +1,3 @@
-//export function extend<First, Second>(first: First, second: Second): First & Second {
-//    const result: Partial<First & Second> = {};
-//    for (const prop in first) {
-//        (<First>result)[prop] = first[prop];
-//    }
-
-//    for (const prop in second) {
-//        if (second.hasOwnProperty(prop)) {
-//            (<Second>result)[prop] = second[prop];
-//        }
-//    }
-//    return <First & Second>result;
-//}
-
-
-
 export interface ProgrammerEmployee {
     firstName: string; 
     lastName: string;
@@ -24,4 +8,3 @@ export interface ProgrammerEmployee {
 export interface BillableResource{
     hourlyRate : number;
 }
-
